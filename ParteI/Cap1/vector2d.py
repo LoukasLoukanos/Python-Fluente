7ef4676e8aff6c0067f206f356a52de81732c7a9
Lucas Chagas Ribeiro <eipi1igual0@gmail.com>
2022-04-04 19:56:54 -0300

    Emulando tipos numéricos.......pág.35

--- ParteI/Cap1/vector2d.py
+++ ParteI/Cap1/vector2d.py
@@ -1,16 +1,20 @@
+from audioop import add, mul
+from ctypes.wintypes import PINT
 from math import hypot
+from pickle import TRUE
 
 class Vector:
 
-    def __init__(self, x=0, y=0):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
 
     def __repr__(self):
-        return 'Vector(%r, %r)' % (self.x, self.y)
+        return 'Vector(%s, %s)' % (self.x, self.y)
 
+    #método abs para calcular a magnitude("hipotenusa") de um vetor bidimencional (x, y)
     def __abs__(self):
-        return hypot(self.x, self.y)
+        return hypot(self.x, self.y) #>>> hypot(3.0, 4.0) → hp=5.0
 
     def __bool__(self):
         return bool(abs(self))
@@ -22,3 +26,19 @@ class Vector:
 
     def __mul__(self, scalar):
         return Vector(self.x * scalar, self.y * scalar)
+
+v = Vector(3, 4)
+repr_ = repr(v)
+print("__repr__:", repr_)
+
+abs_ = abs(v)
+print("__abs__:", abs_)
+
+boll_ = bool(v)
+print("__boll__:", boll_)
+
+add_ = add(v, 5)
+print("__add__:", add_)#TypeError: add expected 3 arguments, got 2 ???????????????
+
+mul_ = mul(v, 5)
+print("__mul__:", mul_)#TypeError: add expected 3 arguments, got 2 ???????????????
