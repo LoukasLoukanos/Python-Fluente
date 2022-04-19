deb27b9650ddf898af87ee8009c46457d8916e17
Lucas Chagas Ribeiro <eipi1igual0@gmail.com>
2022-04-19 20:46:10 -0300

    Visão geral das sequências embutidas.......pág.45

---
+++ ParteII/Cap2/sequencias_embutidas.py
@@ -0,0 +1,20 @@
+
+'''
+
+__________________IMUTÁVEIS("superclasse/pai")__________________
+CONTAINER (armazenam itens de tipos diferentes) : tuple
+SIMPLES (armazenam itens de um só tipo)         : str, bytes
+
+
+
+__________________MUTÁVEIS("subclasse/herança")__________________
+CONTAINER (armazenam itens de tipos diferentes) : list, collections.deque
+SIMPLES (armazenam itens de um só tipo)         : bytearray, array.array, memoryview      
+
+
+
+
+
+Como um exemplo, o tipo mais básico de sequência é list, um container mutável.
+
+'''
