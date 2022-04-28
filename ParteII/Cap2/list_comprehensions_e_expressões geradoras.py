4e679ab072db7e7039a6c2f253ee87af3e27434f
Lucas Chagas Ribeiro <eipi1igual0@gmail.com>
2022-04-28 20:23:42 -0300

    Expressões geradoras.......pág.50

--- ParteII/Cap2/list_comprehensions_e_expressões geradoras.py
+++ ParteII/Cap2/list_comprehensions_e_expressões geradoras.py
@@ -8,24 +8,39 @@
 
 '''
 
-#_________________________________________________________________
-#lista de códigos Unicode(codepoints) sem usar list comprehension:
-symbols = '$)!@|'
+#________________________________________
+# Sem usar list comprehension(listcomps):
+from array import array
+from msilib.schema import ODBCAttribute
+
+symbols = '$)!@|' # códigos Unicode(codepoints)
 codes = []
 for symbol in symbols:
     codes.append(ord(symbol))
+print("Sem usar list comprehension:", codes)
 
-print("sem uasr list comprehension:", codes)
+#______________________________________
+# Usando list comprehension(listcomps):
+symbols2 = '$)!@|' # códigos Unicode(codepoints)
+codes2 = [ord(symbol2) for symbol2 in symbols2] # list comprehension
+print("Usando list comprehension:  ", codes2)
 
-#lista de códigos Unicode(codepoints) usando list comprehension:
-symbols2 = '$)!@|'
-codes2 = [ord(symbol2) for symbol2 in symbols2]#list comprehension
+#___________________________________
+# Usando expressão geradora(genexp):
+symbols = '$)!@|'
+t = tuple(ord(symbol) for symbol in symbols) # com um argumento não é necessário suplicar parênteses para a expressão geradora
+import array
+a = array.array('I', (ord(symbol) for symbol in symbols)) # com mais de um argumento é necessário usar parênteses nas expressões geradoras
+print("Usando expressão geradora duplicando parênteses", t, " Sem duplicar parênteses", a)
 
-print("usando list comprehension:  ", codes2)
+colors = ['black', 'white']
+sizes = ['S', 'M', 'L']
+for tshirt in ('%s %s' % (c, s) for c in colors for s in sizes): # genexp 
+    print("Usando expressão geradora em laço for", tshirt) # graças à expressão geradora é gerado uma saída que não precisa ser armazenada na memória, isso evitou o custo de criar uma lista de itens somente para alimentar o laço for.
 
 #_________________________________________________________________________________________
-#em python 3 as list comprehensions e as epressões geradoras têm seu próprio escopo local,
+# Em python 3 as list comprehensions e as epressões geradoras têm seu próprio escopo local,
 x = 'ABC'
-dummy = [ord(x) for x in x]#list comprehension possui escopo local,
-print(x)#por isso, o valor de x foi preservado,
-print(dummy)#e a list comprehension gera a lista esperada.
+dummy = [ord(x) for x in x] # list comprehension possui escopo local,
+print("Valor da variável preservada fora do escopo", x) # por isso, o valor de x foi preservado,
+print("Valor da variável modificada apenas dentro do escopo", dummy) # e a list comprehension gera a lista esperada.
