b37368acc59b7a68973cfcfd5ddf5c70b77ff8ef
Lucas Chagas Ribeiro <eipi1igual0@gmail.com>
2022-06-22 21:01:14 -0300

    Desempacotamento de tuplas aninhadas.......pág.55

--- ParteII/Cap2/tuplas.py
+++ ParteII/Cap2/tuplas.py
@@ -28,7 +28,8 @@ for country, _ in traveler_ids:# O laço for sabe como obter os itens de uma tup
     print('    5° exemplo: ', type(country), country)#output:<class 'str'> USA, <class 'str'> BRA, <class 'str'> ESP
 
 
-#_________________________Desempacotamento de tuplas ou desempacotamento de iteráveis - pág 53 e 54_________________________
+
+#_________________________Desempacotamento de tuplas ou desempacotamento de iteráveis e capturando itens excedentes com asterisco * - pág 53, 54 e 55_________________________________
 
 #atribuição paralela:
 latitude, longitude = lax_coordinates
@@ -38,7 +39,30 @@ latitude, longitude = longitude, latitude
 #revertendo:
 latitude, longitude = latitude, longitude
 
-#prefixar um argumento com um asterisco ao chamar uma função:
+#prefixar um argumento com um asterisco * ao chamar uma função:
 divmod(20, 8)
 t = (20, 8)
-divmod(*t)#o asterisco serve parainformar que a variável contém todos os parâmetros exigidos por divmod, sem precisar separá-los por vírgulas.
+divmod(*t)#o prefixo asterisco * serve para informar que a variável contém todos os parâmetros exigidos por divmod, sem precisar separá-los por vírgulas.
+
+#atribuição paralela com o prefixo asterisco *
+a, *body, c, d = range(5) # → (a=0, [body = 1, 2], c=3, d=4)
+print(a, *body, c, d) # output: 0, 1, 2, 3, 4
+
+
+#_________________________Desempacotamento de tuplas aninhadas ou desempacotamento de iteráveis aninhadas - pág 55____________________________________________________________________
+
+metro_areas = [
+    ('Tokyo', 'JP', 36.933, (35.689722, 139.691667)),   # a tupla aninhada é um par de coordenadas
+    ('Delhi NCR', 'IN', 21.935, (28.613889, 77.208889)),
+    ('Mexico City', 'MX', 20.142, (19.433333, -99.133333)),
+    ('New York-Newark', 'US', 20.104, (40.808611, -74.020386)),
+    ('Sao Paulo', 'BR', 19.649, (-23.547778, -46.635833)),
+]
+
+print('{:15} | {:^9} | {:^9}'.format('', 'lat.', 'long.'))
+fmt = '{:15} | {:9.4f} | {:9.4f}'
+for name, cc, pop, (latitude, longitude) in metro_areas:  # desempacotando as coordenadas atribuindo-as à tupla (latitude, longitude)
+    if longitude <= 0: # condições de acesso....
+        print(fmt.format(name, latitude, longitude))
+
+#________________________Tuplas nomeadas - pág 56_________________________________
