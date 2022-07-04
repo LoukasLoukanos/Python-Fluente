4851d90b8d9a45d2872893facb689d016e98378a
Lucas Chagas Ribeiro <eipi1igual0@gmail.com>
2022-07-04 19:54:28 -0300

    Fatiamento multidimensional e reticências.......pág.62

--- ParteII/Cap2/tuplas.py
+++ ParteII/Cap2/tuplas.py
@@ -3,8 +3,8 @@
     •podem ser usadas como listas imutáveis
     •podem ser usadas como registros sem nomes de campos
 '''
-#_____________________________________Tuplas como registros - Pág. 52_____________________________________
-
+#_____________________________________Tuplas como registros - Pág. 52__________________________________________________________________________________________________________________
+print("_______________________________Tuplas como registros - Pág. 52_______________________________")
 # 1° EXEMPLO
 from pyparsing import line
 
@@ -33,7 +33,7 @@ for country, _ in traveler_ids:# O laço for sabe como obter os itens de uma tup
 
 
 #_________________________Desempacotamento de tuplas ou desempacotamento de iteráveis e capturando itens excedentes com asterisco * - pág 53, 54 e 55_________________________________
-
+print("___________________Desempacotamento de tuplas ou desempacotamento de iteráveis e capturando itens excedentes com asterisco * - pág 53, 54 e 55___________________")
 #atribuição paralela:
 latitude, longitude = lax_coordinates
 
@@ -53,7 +53,7 @@ print(a, *body, c, d) # output: 0, 1, 2, 3, 4
 
 
 #_________________________Desempacotamento de tuplas aninhadas ou desempacotamento de iteráveis aninhadas - pág 55____________________________________________________________________
-
+print("___________________Desempacotamento de tuplas aninhadas ou desempacotamento de iteráveis aninhadas - pág 55___________________")
 metro_areas = [
     ('Tokyo', 'JP', 36.933, (35.689722, 139.691667)),   # a tupla aninhada é um par de coordenadas
     ('Delhi NCR', 'IN', 21.935, (28.613889, 77.208889)),
@@ -68,15 +68,47 @@ for name, cc, pop, (latitude, longitude) in metro_areas:  # desempacotando as co
     if longitude <= 0: # condições de acesso....
         print(fmt.format(name, latitude, longitude))
 
-#________________________Tuplas nomeadas - pág 56_________________________________
-from collections import namedtuple
+#________________________Tuplas nomeadas - pág 56 à 58_________________________________________________________________________________________________________________________________
+print('__________________Tuplas nomeadas - pág 56 à 58__________________')
+from collections import namedtuple #namedtuples contêm chaves como hash para um valor específico, oferecendo suporte ao acesso do valor pela chave[key] e pela iteração[x]
 City = namedtuple('City', 'name country population coordinates')
-tokyo = City('tokio', 'JP', population=36.933, coordinates=(36.689722, 139.691667))
-print("—————————————————————\n", tokyo.population)
-print(tokyo.coordinates)
-print(tokyo[1],"\n_____________________")
+tokyo = City('tokyo', 'JP', population=36.933, coordinates=(36.689722, 139.691667))
+print("acesso por chaves[keys]:\n", tokyo.name, tokyo.country, tokyo.population, tokyo.coordinates)
+#↑ou↓
+print("acesso por iteração[x]:\n", tokyo[0], tokyo[1], tokyo[2], tokyo[3], "\n......................") 
+''' Output:
+        —————————————————————
+        acesso por chaves[keys]:
+         tokyo JP 36.933 (36.689722, 139.691667)
+        acesso por iteração[x]:
+         tokyo JP 36.933 (36.689722, 139.691667) 
+        —————————————————————
+'''
+print('Retorno de _fields: ', City._fields) # _fields retorna uma tupla com os nomes das [chaves] dos valores da classe namedtuple definida  | output:('name', 'country', 'population', 'coordinates')
+LatLong = namedtuple('LatLong', 'Lat Long') #Lat será a shave[key] do valor 28.613889; e Long será a chave[key] do valor 77.208889 ↓↓↓
+delhi_data = ('Delhi NCR', 'IN', 21935, LatLong(28.613889, 77.208889))# LatLong(valor da chave [Lat], valor da chave[Long]) ↑↑↑
+delhi = City._make(delhi_data) # _make() permite instanciar uma tupla nomeada a partir de um iterável. Os valores das chaves [Lat] e [Long] da namedtuple LatLong() serão, também, os valores da chave [coordinate] da namedtuple City.
+print('Retorno de _asdict(): ', delhi._asdict()) # _asdict() retorna um collections.OrderedDict , chaves e valores... | output: Retorno de _asdict():  {'name': 'Delhi NCR', 'country': 'IN', 'population': 21935, 'coordinates': LatLong(Lat=28.613889, Long=77.208889)}
+for key, value in delhi._asdict().items():
+    print('hash [',key, ']', ' = ', value)
+    ''' Output:
+            hash [ name ]  =  Delhi NCR
+            hash [ country ]  =  IN
+            hash [ population ]  =  21935
+            hash [ coordinates ]  =  LatLong(Lat=28.613889, Long=77.208889)
+    '''
 
 #________________________Fatiamento - pág 59 à 62_____________________________________________________________________________________________________________________________________
+print("__________________Fatiamento - pág 59 à 62__________________")
+l = [10, 20, 30, 40, 50, 60]
+print('l[:2] = ', l[:2]) #output: [10, 20]
+print('l[2:] = ', l[2:]) #output: [30, 40, 50, 60]
+
+s = 'Bicycle'
+print('s[::3] = ', s[::3]) #output: Bye
+print('s[::-1] = ', s[::-1]) #output: elcyciB
+print('s[::-2] = ', s[::-2]) #output: eccB
+
 invoice = """
 0.....6.................................40........52...55........
 1909 Pimoroni PiBrella                      $17.50    3    $52.50
