b9fbc254836532b1a819f8ad4548d03a94d4c50d
Lucas Chagas Ribeiro <eipi1igual0@gmail.com>
2022-09-08 20:55:52 -0300

    Método __missing__.......pág.104

--- ParteII/Cap3/pag103_metodo_missing.py
+++ ParteII/Cap3/pag103_metodo_missing.py
@@ -4,7 +4,9 @@
     O diferencial do tipo de mapeamento defaultdict é que ele é capaz de devolver valores predefinidos quando chaves são ausentes:
     (O método especial __missing__ é o mecanismo que faz defaultdict funcionar(chamando default_factory) para essa finalidade.)
 '''
-class StrKeyDict0(dict):  # StrKeyDict0 herda de dict
+
+# exemplo com uma classe que herda de dict_____________________________________
+class ExemploSimples(dict):  # ExemploSimples herda de dict
 
     def __missing__(self, key):
         if isinstance(key, str):  # Verifica se key já é uma str. Se for e estiver ausente, gera KeyError.
@@ -21,9 +23,9 @@ class StrKeyDict0(dict):  # StrKeyDict0 herda de dict
         return key in self.keys() or str(key) in self.keys()  # Procura a chave não modificada (a instância pode conter chaves que não sejam str) e, em seguida, procura uma str criada a partir da chave.
 
 
-'''____________________testes para StrKeyDict0()____________________
+'''___testes para ExemploSimples()_________
 
-d = StrKeyDict0([('2', 'two'), ('4', 'four')])
+d = ExemploSimples([('2', 'two'), ('4', 'four')])
 d['2']
     output: 'two'
 d[4]
@@ -48,4 +50,71 @@ d.get(1, 'N/A')
 1 in d
     output: False
 
+'''
+
+import collections
+# exemplo com uma classe que herda de UserDict_____________________________________
+# Graças a UserDict, a classe ExemploAvancado é menor que ExemploSimples e sempre converte chaves que não são strings para str — na inserção, na atualização e na consulta.
+class ExemploAvancado(collections.UserDict):  # A classe ExemploAvancado estende de UserDict
+
+    def __missing__(self, key):  # __missing_ é exatamente como no exemplo anterior
+        if isinstance(key, str): # Verifica se key já é uma str. Se for e estiver ausente, gera KeyError.
+            raise KeyError(key)
+        return self[str(key)]
+
+    def __contains__(self, key):
+        return str(key) in self.data  # __contais__ é mais simples que no exemplo anterior: pode-se supor que todas as chaves armazenadas sejam str e consultar self.data em vez de chamar senf.keys() como fieto em ExemploSimples
+
+    def __setitem__(self, key, item):
+        self.data[str(key)] = item   # __setitem__ converte qualquer key para uma str. Esse método é mais fácil de sobrescrever quando podemos delegar ao atributo self.data.
+
+
+'''___testes para ExemploAvancado()_________
+
+d = ExemploAvancado([(2, 'two'), ('4', 'four')])
+sorted(d.keys())
+    output: ['2', '4']
+
+
+d['2']
+    output: 'two'
+d[4]
+    output: 'four'
+d[1]
+    output:
+        Traceback (most recent call last):
+          ...
+        KeyError: '1'
+
+
+d.get('2')
+    output: 'two'
+d.get(4)
+    output: 'four'
+d.get(1, 'N/A')
+    output: 'N/A'
+
+
+2 in d
+    output: True
+1 in d
+    output: False
+
+
+d[0] = 'zero'
+d['0']
+    output: 'zero'
+
+
+d.update({6:'six', '8':'eight'})
+sorted(d.keys())
+    output: ['0', '2', '4', '6', '8']
+d.update([(10, 'ten'), ('12', 'twelve')])
+sorted(d.keys())
+    output: ['0', '10', '12', '2', '4', '6', '8']
+d.update([1, 3, 5])
+    output: 
+        Traceback (most recent call last):
+          ...
+        TypeError: 'int' object is not iterable
 '''
