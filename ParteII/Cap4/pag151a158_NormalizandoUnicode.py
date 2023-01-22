86c6e7c5f1796af2bb24409c8cfcc99859bbdf4c
Lucas Chagas Ribeiro <eipi1igual0@gmail.com>
2023-01-21 23:27:25 -0300

    Pág. 151 à 158

--- ParteII/Cap4/pag151a158_NormalizandoUnicode.py
+++ ParteII/Cap4/pag151a158_NormalizandoUnicode.py
@@ -1,21 +1,49 @@
 # Normalizando Unicode para comparações mais seguras:
 
 from unicodedata import normalize
-# O primeiro argumento de normalize suporta quatro trings: 
-# 'NFC' → compõe os códigos Unicode de modo a gerar a menor string equivalente
-# 'NFD' → decompõe expandindo os caracteres compostos em caracteres básicos, e separa os caracteres combinados
-# 'NFKC' → a letra k representa 'compatibility' (compatibilidade)
-# 'NFKD' → a letra k representa 'compatibility' (compatibilidade)
+'''
+ O primeiro argumento de normalize suporta quatro trings: 
+    'NFC' → compõe de modo equivalente... Gera a menor string equivalente aos códigos Unicode
+    'NFD' → decompõe de modo equivalente... Expande os caracteres compostos em caracteres básicos, separando os caracteres combinados
+    'NFKC' → compõe de modo compatível ... A letra k representa 'compatibility' (compatibilidade), portanto não promete equivalência
+    'NFKD' → decompõe de modo compatível... A letra k representa 'compatibility' (compatibilidade), portanto não promete equivalência
+ obs: normalize não formata (NORMALIZA), portanto NFKC e NFKD podem distorcer informações.
+'''
 
+# exemplos________________________________________________________________________________________________
+
+# Usando (NFC) → "Compõe de modo equivalente":
+cof = 'cafe\u0301'
+cof = normalize('NFC', cof)
+    # output: café
+
+# Usando (NFD) → "Decompõe de modo equivalente":
+cof = normalize('NFD', cof)
+    # output: cafe\u0301
+
+
+# Usando (NFKC) → "Compõe de modo compatível"
+half = '⅛'
+half = normalize('NFKC', half)
+    #output: 1/2 → a função normalize não sabe nada sobre formatação, por isso essa saída
+
+# Usando (NFKD) → "Decompõe de modo compatível"
+half = normalize('NFKD', half)
+    #output: ⅛ → a função normalize não sabe nada sobre formatação, por isso essa saída
+
+
+
+# usando com funções______________________________________________________________________________________
 def nfc_equal(str1, str2):
+    # casefold() não leva em conta as diferenças entre as letras minúsculas e maiúsculas (case-insensitive)
     return normalize('NFC', str1) == normalize('NFC', str2)
 
 def fold_equal(str1, str2):
-    return (normalize('NFC', str1).casefold() == normalize('NFC', str2).casefold()) # casefold() não leva em conta as diferenças entre as letras minúsculas e maiúsculas (case-insensitive)
+    # casefold() não leva em conta as diferenças entre as letras minúsculas e maiúsculas (case-insensitive)
+    return (normalize('NFC', str1).casefold() == normalize('NFC', str2).casefold())
 
 
-# Usando (NFC), com case-sensitive :
-
+# Usando (NFC) → "Compõe":
 s1 = 'café'
 s2 = 'cafe\u0301'
 s1 == s2
@@ -25,18 +53,17 @@ nfc_equal(s1, s2)
 nfc_equal('A', 'a')
     #output: False → (não há o que gerar de nova string equivalente, pois 'a' não é a composição de 'A', ou vice-versa, em Unicode)
 
-# Usando (NFC) com case-insensitive:
 
+# Usando (NFC) → "Compõe":
 s3 = 'Straße'
 s4 = 'strasse'
 s3 == s4
     #output: False
 nfc_equal(s3, s4)
     #output: False → (não há o que gerar de nova string equivalente, pois 'ß' não é a composição de 'ss(minúsculo)', ou vice-versa, em Unicode)
-
 fold_equal(s3, s4)
     #output: True → pois (casefold() não leva em conta as diferenças entre as letras minúsculas e maiúsculas (case-insensitive))
 fold_equal(s1, s2)
-    #output: True
-fold_equal('A', 'a')
+    #output: True 
+fold_equal('A', 'a') # retorno (case-insensitive)
     #output: True
