96d24cb317d67d285aef53c672e009c70534ef09
Lucas Chagas Ribeiro <eipi1igual0@gmail.com>
2023-05-10 19:25:02 -0300

    Pág. 205 à 216

--- ParteIII/Cap6/Pag205a216/pag210_function_strategy.py
+++ ParteIII/Cap6/Pag205a216/pag210_function_strategy.py
@@ -32,16 +32,16 @@ class Order:  # the Context
         if self.promotion is None:
             discount = 0
         else:
-            discount = self.promotion(self)  # <1>
+            discount = self.promotion(self)  # Para calcular um desconto, basta chamar a função self.promotion()
         return self.total() - discount
 
     def __repr__(self):
         fmt = '<Order total: {:.2f} due: {:.2f}>'
         return fmt.format(self.total(), self.due())
 
-# <2>
+# não há classe abstrata
 
-def fidelity_promo(order):  # <3>
+def fidelity_promo(order):  # cada estratégia é uma função
     """5% discount for customers with 1000 or more fidelity points"""
     return order.total() * .05 if order.customer.fidelity >= 1000 else 0
 
@@ -63,28 +63,24 @@ def large_order_promo(order):
     return 0
 
 
-"""
-# BEGIN STRATEGY_TESTS
+joe = Customer('John Doe', 0) # mesma configuração de teste do exemplo pag208_classic_strategy.py
+ann = Customer('Ann Smith', 1100)
 
-    >>> joe = Customer('John Doe', 0)  # <1>
-    >>> ann = Customer('Ann Smith', 1100)
-    >>> cart = [LineItem('banana', 4, .5),
-    ...         LineItem('apple', 10, 1.5),
-    ...         LineItem('watermellon', 5, 5.0)]
-    >>> Order(joe, cart, fidelity_promo)  # <2>
-    <Order total: 42.00 due: 42.00>
-    >>> Order(ann, cart, fidelity_promo)
-    <Order total: 42.00 due: 39.90>
-    >>> banana_cart = [LineItem('banana', 30, .5),
-    ...                LineItem('apple', 10, 1.5)]
-    >>> Order(joe, banana_cart, bulk_item_promo)  # <3>
-    <Order total: 30.00 due: 28.50>
-    >>> long_order = [LineItem(str(item_code), 1, 1.0)
-    ...               for item_code in range(10)]
-    >>> Order(joe, long_order, large_order_promo)
-    <Order total: 10.00 due: 9.30>
-    >>> Order(joe, cart, large_order_promo)
-    <Order total: 42.00 due: 42.00>
+cart = [LineItem('banana', 4, .5), LineItem('apple', 10, 1.5), LineItem('watermellon', 5, 5.0)]
 
-# END STRATEGY_TESTS
-"""
+Order(joe, cart, fidelity_promo)  # para aplicar uma estratégia de desconto em um Order, basta passar a função de promoção como argumento
+#output: <Order total: 42.00 due: 42.00>
+Order(ann, cart, fidelity_promo)
+#output: <Order total: 42.00 due: 39.90>
+
+banana_cart = [LineItem('banana', 30, .5), LineItem('apple', 10, 1.5)]
+
+Order(joe, banana_cart, bulk_item_promo)  # uma função diferente de promoção é usada nesse caso
+#output: <Order total: 30.00 due: 28.50>
+
+long_order = [LineItem(str(item_code), 1, 1.0) for item_code in range(10)]
+
+Order(joe, long_order, large_order_promo)
+#output: <Order total: 10.00 due: 9.30>
+Order(joe, cart, large_order_promo)
+#output: <Order total: 42.00 due: 42.00>
