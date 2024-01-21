d332a282aef81f8c72273f59a35e598f12881bb0
Lucas Chagas Ribeiro <eipi1igual0@gmail.com>
2024-01-21 13:45:07 -0300

    Pág. 208 à 210

--- ParteIII/Cap6/pag208_classic_strategy.py
+++ ParteIII/Cap6/pag208_classic_strategy.py
@@ -1,44 +1,7 @@
 # Padrão de Projeto Clássico Baseado em Classe (Strategy pattern clássic implementation):
 
 from abc import ABC, abstractmethod
-from collections import namedtuple
-
-Customer = namedtuple('Customer', 'name fidelity')
-
-class LineItem:
-
-    def __init__(self, product, quantity, price):
-        self.product = product
-        self.quantity = quantity
-        self.price = price
-
-    def total(self):
-        return self.price * self.quantity
-
-
-class Order:
-
-    def __init__(self, customer, cart, promotion=None):
-        self.customer = customer
-        self.cart = list(cart)
-        self.promotion = promotion
-
-    def total(self):
-        if not hasattr(self, '__total'):
-            self.__total = sum(item.total() for item in self.cart)
-        return self.__total
-
-    def due(self):
-        if self.promotion is None:
-            discount = 0
-        else:
-            discount = self.promotion.discount(self)
-        return self.total() - discount
-
-    def __repr__(self):
-        fmt = '<Order total: {:.2f} due: {:.2f}>'
-        return fmt.format(self.total(), self.due())
-
+from pag208_contexto import *
 
 #_________↓Padrão de Projeto Clássico Baseado em Classe↓__________________
 class Promotion(ABC):  # the Strategy: an Abstract Base Class
