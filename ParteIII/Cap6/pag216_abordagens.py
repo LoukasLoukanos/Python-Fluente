cc8dda179cf44a330d742cbc5eaf594c693879e8
Lucas Chagas Ribeiro <eipi1igual0@gmail.com>
2024-01-21 13:50:16 -0300

    Pág. 213 à 221

--- ParteIII/Cap6/pag216_abordagens.py
+++ ParteIII/Cap6/pag216_abordagens.py
@@ -1,9 +1,11 @@
-import inspect
+# Possibilidades dos benefícios disponibilizados apenas para o padrão de projeto baseado em função
 
+import inspect
+from pag208_contexto import *
 from pag210_function_strategy import *
 import pag210_function_strategy
 
-# abordagem I___________________________
+# Possibilidade I___________________________
 promos1 = [fidelity_promo, bulk_item_promo, large_order_promo] # "promos1" é definida como uma lista que contém três funções
 
 def best_promo1(order): # define uma função chamada "best_promo1" que recebe um pedido como argumento
@@ -16,7 +18,7 @@ def best_promo1(order): # define uma função chamada "best_promo1" que recebe u
 
 
 
-# abordagem II___________________________
+# Possibilidade II___________________________
 '''
 a variável "promos2" é definida como uma lista de funções. Ele usa a função 
 "globals" para obter todas as variáveis globais do programa e, em seguida, 
@@ -37,7 +39,7 @@ def best_promo2(order): # recebe um pedido como argumento.
 
 
 
-# abordagem III___________________________
+# Possibilidade III___________________________
 '''
 a variável "promos3" é definida como uma lista de funções. Ele usa a função 
 "inspect.getmembers" para obter todos os membros da classe ou módulo "promotions" 
@@ -52,3 +54,46 @@ def best_promo3(order): # recebe um pedido como argumento.
     aplicá-las ao pedido. O desconto resultante é retornado.
     '''
     return max(promo(order) for promo in promos3)
+
+
+#_________↓Uso de decorador (substitui os códigos acima de forma eficiente)↓________________
+# Obs: o uso de decoradores não substitui o Padrão de Projeto Baseado em Função 
+# (Strategy pattern function-based implementation), mas sim a eficiência.
+
+promos = []  # A lista promos para armazenar as estratégias de promoção disponíveis
+
+# Função de decorador para adicionar funções de promoção à lista promos
+def promotion(promo_func):  # O decorador promotion devolve promo_func inalterada após adicioná-la à lista promos.
+    print('→ no tempo de importação (import time):\nfunção decorada: %s\n' % promo_func)
+    promos.append(promo_func)
+    return promo_func
+
+# Decoradores aplicados a funções de promoção específicas
+@promotion  # Qualquer função decorada com @promotion será adicionada a promos.
+def fidelity(order):
+    """5% discount for customers with 1000 or more fidelity points"""
+    return order.total() * .05 if order.customer.fidelity >= 1000 else 0
+
+@promotion
+def bulk_item(order):
+    """10% discount for each LineItem with 20 or more units"""
+    discount = 0
+    for item in order.cart:
+        if item.quantity >= 20:
+            discount += item.total() * .1
+    return discount
+
+@promotion
+def large_order(order):
+    """7% discount for orders with 10 or more distinct items"""
+    distinct_items = {item.product for item in order.cart}
+    if len(distinct_items) >= 10:
+        return order.total() * .07
+    return 0
+
+# Função para selecionar a melhor promoção disponível
+def best_promo(order):  # Nenhuma alteração é necessária em best_promos, pois ela baseia-se na lista promos.
+    """Select best discount available"""
+    return max(promo(order) for promo in promos)
+
+#_________↑Uso de decorador (substitui o código acima de forma eficiente)↑________________
